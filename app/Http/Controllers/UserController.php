8cc720307e9f28f6b23b8ff34658ce70311727c2
gustiaryapriandana31 <gustiaryapriandana@gmail.com>
2024-08-03 18:07:40 +0700

    feat: adding create, update, show, and delete feature for rest api data users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -12,8 +14,102 @@ class UserController extends Controller
         $users = User::all();
 
         return response()->json([
-            "message" => "Successfully get all users data",
+            "message" => "All users data retrieved successfully",
             "data" => $users
+        ], Response::HTTP_OK);
+    }
+
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            "name" => "required|string",
+            "username" => "required|string",
+            "email" => "required|email|email:rfc, dns|unique:users,email",
+            "password" => "required|string"
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                "message" => "Error Validation Error",
+                "errors" => $validator->errors()
+            ], Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+        $validatedData = $validator->validate();
+        $validatedData["password"] = bcrypt($validatedData["password"]);
+
+        try {
+            $createdUser = User::create($validatedData);
+        } catch(Exception $e) {
+            return response()->json([
+                "message" => "Failed to create user",
+                "error" => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            "message" => "User data created successfully",
+            "data" => $createdUser
+        ], Response::HTTP_CREATED);
+    }
+
+    public function show($userId) {
+        $user = User::findOrFail($userId);
+        return response()->json([
+            "message" => "User data retrieved successfully",
+            "data" => $user
+        ], RESPONSE::HTTP_OK);
+    }
+
+    public function update(Request $request, $userId) {
+        $validator = Validator::make($request->all(), [
+            "name" => "string",
+            "username" => "string",
+            "email" => "email|email:rfc, dns|unique:users,email",
+            "password" => "string"
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                "message" => "Error Validation Error",
+                "errors" => $validator->errors()
+            ], Response::HTTP_NOT_ACCEPTABLE);
+        }
+
+        $validatedData = $validator->validate();
+        if(isset($validatedData["password"])) {
+            $validatedData["password"] = bcrypt($validatedData["password"]);
+        }
+
+        try {
+            $userData = User::findOrFail($userId);
+            $userData->update($validatedData);
+        } catch(Exception $e) {
+            return response()->json([
+                "message" => "Failed to create user",
+                "error" => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            "message" => "User data updated successfully",
+            "data" => $userData
+        ], Response::HTTP_CREATED);
+    }
+
+    public function destroy($userId) {
+        $user = User::findOrFail($userId);
+
+        try {
+            $user->delete();
+        } catch(Exception $e) {
+            return response()->json([
+                "message" => "Failed to delete user",
+                "error" => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        } 
+
+        return response()->json([
+            "message" => "User data with id {$userId} deleted successfully",
         ], Response::HTTP_OK);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,11 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', [UserController::class, 'index']);
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::post('/users/{userId}', [UserController::class, 'show']);
+Route::put('/users/{userId}', [UserController::class, 'update']);
+Route::delete('/users/{userId}', [UserController::class, 'destroy']);
 
 
 
